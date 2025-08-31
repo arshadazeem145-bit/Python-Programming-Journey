33fe7b35a62a01deb1558a21ce2fd5cf4ea58111
Arshad Azeem <arshadazeem145@gmail.com>
2025-08-31 23:31:46 +0500

    my first program

---
+++ test.py
@@ -0,0 +1,13 @@
+
+
+print("---------------")
+print("Welcome ARSHAD AZEEM")
+print("---------------")
+
+a=10
+b=5
+print("SUM= ",a+b)
+print("SUBTRACTION= ",a-b)
+print("MULTIPLICATION= ",a*b)
+print("DIVIDE= ",a/b)
+print("REMAINDER= ",a%b)
